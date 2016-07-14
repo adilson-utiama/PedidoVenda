f2d08ea749680e6b0292ebbd9efec95aac39619a
Adilson <asu2u@yahoo.com.br>
2016-07-13 23:22:28 -0300

    Criando um produtor de EntityManagerFactory

--- src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
+++ src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
@@ -1,10 +1,16 @@
 package com.algaworks.pedidovenda.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import com.algaworks.pedidovenda.model.Categoria;
 import com.algaworks.pedidovenda.model.Produto;
 
 @Named
@@ -13,7 +19,11 @@ public class CadastroProdutoBean implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Inject
+	private EntityManager manager;
+	
 	private Produto produto;
+	private List<Categoria> categoriaRaizes;
 	
 	public CadastroProdutoBean(){
 		produto = new Produto();
@@ -23,8 +33,18 @@ public class CadastroProdutoBean implements Serializable{
 		//throw new RuntimeException("Não é possivel salvar produto, função não implementada!");
 	}
 	
+	public void inicializar(){
+		System.out.println("Inicializando lista de Categorias do banco...");
+		categoriaRaizes = manager.createQuery("from Categoria", Categoria.class).getResultList();
+		
+	}
+	
 	
 	public Produto getProduto() {
 		return produto;
+	}
+	
+	public List<Categoria> getCategoriaRaizes() {
+		return categoriaRaizes;
 	}
 }

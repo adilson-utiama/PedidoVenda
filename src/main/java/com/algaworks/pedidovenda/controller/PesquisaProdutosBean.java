7b0124882a4b2932b7529bc8d1cd99cbe605fb9d
Adilson <asu2u@yahoo.com.br>
2016-07-15 01:19:19 -0300

    Impementando a pesquisa de produtos

--- src/main/java/com/algaworks/pedidovenda/controller/PesquisaProdutosBean.java
+++ src/main/java/com/algaworks/pedidovenda/controller/PesquisaProdutosBean.java
@@ -1,25 +1,39 @@
 package com.algaworks.pedidovenda.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import com.algaworks.pedidovenda.model.Produto;
+import com.algaworks.pedidovenda.repository.ProdutoRepository;
+import com.algaworks.pedidovenda.repository.filter.ProdutoFilter;
 
 @Named
 @RequestScoped
 public class PesquisaProdutosBean {
 
-	private List<Integer> produtosFiltrados;
+	@Inject
+	private ProdutoRepository produtos;
+	
+	private ProdutoFilter filtro;
+	private List<Produto> produtosFiltrados;
+	
 	
 	public PesquisaProdutosBean() {
-		produtosFiltrados = new ArrayList<>();
-		for (int i = 0; i < 50; i++) {
-			produtosFiltrados.add(i);
-		}
+		filtro = new ProdutoFilter();
+	}
+		
+	public void pesquisar(){
+		produtosFiltrados = produtos.filtrados(filtro);
 	}
 	
-	public List<Integer> getProdutosFiltrados() {
+	public List<Produto> getProdutosFiltrados() {
 		return produtosFiltrados;
+	}
+	
+	public ProdutoFilter getFiltro() {
+		return filtro;
 	}
 }
